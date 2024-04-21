318b488859950e1900b9d257de9e148bba130c29
Avinash4305 <145126833+Avinash4305@users.noreply.github.com>
2024-04-21 18:02:40 +0530

    created addEmployee class to enter the detail of the new employee in the hotel and linked the class with database to save the employee details

--- src/hotel/management/system/addEmployee.java
+++ src/hotel/management/system/addEmployee.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.ResultSet;
 
 public class addEmployee extends JFrame implements ActionListener {
 
