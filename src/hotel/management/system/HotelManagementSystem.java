9651925fbe2b0a92f09a1e13cf25fb859ae4f332
Avinash4305 <145126833+Avinash4305@users.noreply.github.com>
2024-04-21 11:54:53 +0530

    Created home page in the hotel management system project

---
+++ src/hotel/management/system/HotelManagementSystem.java
@@ -0,0 +1,38 @@
+package hotel.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HotelManagementSystem extends JFrame {
+
+    HotelManagementSystem() {
+
+        setSize(1366, 565); // setting the size of the login frame
+        setLocation(42, 152);
+        setLayout(null);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/first.jpg"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0,0,1366,565);//location x, location y, length, breadth
+        add(image);
+
+        JLabel text = new JLabel("HOTEL MANAGEMENT SYSTEM");
+        text.setBounds(20,430,1000,90);
+        text.setFont(new Font("serif",Font.PLAIN,50));
+        text.setForeground(Color.WHITE); // Color class needs to import awt pacakge
+        image.add(text);
+
+        JButton next = new JButton("Next");
+        next.setBounds(1150,450,150,50);
+        next.setBackground(Color.BLACK);
+        image.add(next);
+
+        setVisible(true);
+
+        }
+
+
+    public static void main(String[] args) {
+        new HotelManagementSystem();
+    }
+}
